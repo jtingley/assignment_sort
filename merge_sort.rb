9aa60bf3bbc3af978d285cbcd2637a8701396437
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-10 12:20:05 -0500

    Begin merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,26 @@
+
+#function mergeSort(array) {
+  #// if the array is one element long, just return it
+  #// mergeSort() the left half of the array
+  #// mergeSort() the right half of the array
+  #// merge() the two halves
+  #// return the merged array
+#}
+
+#function merge(leftArr, rightArr){
+  #// var newArr = [];
+  #// compare leftArr[0] and rightArr[0]
+  #// whichever is smaller, push it onto newArr
+  #// repeat the process, but don't reuse elements,
+  #// until you have pushed all the elements from
+  #// leftArr and rightArr onto newArr in sorted order
+  #// return newArr
+#}
+
+def merge_sort(array)
+  return if array.length == 1
+
+end
+
+def merge(array_1, array_2)
+end
